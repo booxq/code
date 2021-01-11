263e4919254de067496dc79b0e4210439eea355f
xuqing <xuqing.li97@qq.com>
2021-01-11 15:54:48 +0800

    listing11 show addr

--- chapter3/test/main.go
+++ chapter3/test/main.go
@@ -1,9 +1,39 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func foo(slice []int) {
+	slice[0] = 1000
+}
 
 func main() {
-	fmt.Print("helowd")
+
+	var arr = [4]int{1, 2, 3, 4}
+	s1 := arr[0:4]
+	s2 := s1[2:3:3]
+	fmt.Printf("slice s1 : %v\n", s1)
+	fmt.Printf("slice s2 : %v\n", s2)
+	fmt.Println("slice1 cap", cap(s1))
+	fmt.Println("slice2 cap", cap(s2))
+	fmt.Println("slice1 addr=", &s1[2])
+	fmt.Println("slice2 addr=", &s2[0])
+	//s2 = append(s2, 5)
+	foo(s2)
+	fmt.Printf("slice s1 : %v\n", s1)
+	fmt.Printf("slice s2 : %v\n", s2)
+	fmt.Println("slice1 cap", cap(s1))
+	fmt.Println("slice2 cap", cap(s2))
+	fmt.Println("slice1 addr=", &s1[2])
+	fmt.Println("slice2 addr=", &s2[0])
+
+	/*二维切片*/
+
+	// slice := [][]int{{10, 20, 30}, {100, 200, 300}}
+	// fmt.Println("slice", &slice)
+	// fmt.Println("00 addr", &slice[0][0])
+	// fmt.Println("10 addr", &slice[1][0])
+	// fmt.Println("11 addr", &slice[1][1])
+	// slice[0] = append(slice[0], 20)
+	// fmt.Println("00 addr", &slice[0][0])
+
 }
--- chapter5/listing11/listing11.go
+++ chapter5/listing11/listing11.go
@@ -14,6 +14,7 @@ type user struct {
 
 // notify implements a method with a value receiver.
 func (u user) notify() {
+	fmt.Printf("notify %v addr %v:", u.name, &u.name)
 	fmt.Printf("Sending User Email To %s<%s>\n",
 		u.name,
 		u.email)
@@ -21,6 +22,7 @@ func (u user) notify() {
 
 // changeEmail implements a method with a pointer receiver.
 func (u *user) changeEmail(email string) {
+	fmt.Printf("change %s addr %v:\n", u.name, &u.name)
 	u.email = email
 }
 
@@ -29,11 +31,13 @@ func main() {
 	// Values of type user can be used to call methods
 	// declared with a value receiver.
 	bill := user{"Bill", "bill@email.com"}
+	fmt.Printf("user bill addr %v:\n", &bill.name)
 	bill.notify()
 
 	// Pointers of type user can also be used to call methods
 	// declared with a value receiver.
 	lisa := &user{"Lisa", "lisa@email.com"}
+	fmt.Printf("user Lisa addr %v:\n", &lisa.name)
 	lisa.notify()
 
 	// Values of type user can be used to call methods
