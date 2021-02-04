e6da1443c17dd938dd2ebec30b52ae08e3cc4f22
xuqing <xuqing.li97@qq.com>
2021-02-04 15:50:05 +0800

    leen struct and goroutine

--- chapter6/listing01/listing01.go
+++ chapter6/listing01/listing01.go
@@ -11,7 +11,7 @@ import (
 // main is the entry point for all Go programs.
 func main() {
 	// Allocate 1 logical processor for the scheduler to use.
-	runtime.GOMAXPROCS(1)
+	runtime.GOMAXPROCS(4)
 
 	// wg is used to wait for the program to finish.
 	// Add a count of two, one for each goroutine.
